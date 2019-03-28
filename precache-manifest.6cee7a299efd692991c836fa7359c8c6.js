cd50422a1d7b1f61adead8c9f16b806ba92fbb14
Reginald Galang <reguv760@gmail.com>
2019-03-27 19:31:25 -0700

    Updates

---
+++ precache-manifest.6cee7a299efd692991c836fa7359c8c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1c08ff48edc83c15980a9abd9d3971a",
+    "url": "/weather-finder/static/media/nighttime.c1c08ff4.jpg"
+  },
+  {
+    "revision": "a3f33b07d83e354fae139f46f4a9174a",
+    "url": "/weather-finder/static/media/daytime.a3f33b07.jpg"
+  },
+  {
+    "revision": "07c9cb110cbb6996aaf0",
+    "url": "/weather-finder/static/js/runtime~main.a4583ac8.js"
+  },
+  {
+    "revision": "0ce160c7db64e2c77980",
+    "url": "/weather-finder/static/js/main.b26af5cb.chunk.js"
+  },
+  {
+    "revision": "9a2acaaafc9e36d2b040",
+    "url": "/weather-finder/static/js/2.9d78c24b.chunk.js"
+  },
+  {
+    "revision": "0ce160c7db64e2c77980",
+    "url": "/weather-finder/static/css/main.7ffa3400.chunk.css"
+  },
+  {
+    "revision": "9a2acaaafc9e36d2b040",
+    "url": "/weather-finder/static/css/2.4fbdb43d.chunk.css"
+  },
+  {
+    "revision": "260f801b3cda845db4a638e671c4af90",
+    "url": "/weather-finder/index.html"
+  }
+];
